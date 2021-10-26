313b97ff28a344896b2ed4d8347b3cc6a0ca9644
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-26 18:06:17 +0530

    Added: Random check file is added

--- src/com/blp/tictactoegame/TicTacToeGame.java
+++ src/com/blp/tictactoegame/TicTacToeGame.java
@@ -1,5 +1,6 @@
 package com.blp.tictactoegame;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -8,7 +9,11 @@ import java.util.Scanner;
 public class TicTacToeGame {
     //Declaring and initializing variables
     public static char[] board;
-    public static char charter;
+    public static String char1 = "x";
+    public static String char2 = "o";
+    public static char userInput;
+    public static int input;
+    public static int computerChoice;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
@@ -31,49 +36,53 @@ public class TicTacToeGame {
     private static void makeChoice() { //Choice charter to play the game
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter charter 'X' or 'O'");
-        String charter = sc.next();
+        String userInput = sc.next();
+        System.out.println("Player chooses the charter = " + userInput);
+        if (userInput.equals(char1)){
+            System.out.println("Computer will take input= " + char2);
+        }
+        else
+            System.out.println("Computer will take input = " + char1);
     }
 
     private static void showBoard() {
-        for (int i = 1; i <= board.length - 1; i++) {
-            if (i == 3 || i == 6) {
-                System.out.println(board[i]);
-            } else {
-                System.out.print(board[i] + "_" + "|" + "_");
-            }
-        }
+        System.out.println(" " + board[1] + "|" + board[2] + "|" + board[3]);
+        System.out.println("--+--+--");
+        System.out.println(" " + board[4] + "|" + board[5] + "|" + board[6]);
+        System.out.println("--+--+--");
+        System.out.println(" " + board[7] + "|" + board[8] + "|" + board[9]);
     }
 
     private static void move() {
         Scanner sc = new Scanner(System.in);
         System.out.println();
         System.out.println("Enter number between 1 to 9");
-        int number = sc.nextInt();
-        System.out.println("Player got the location on board = " + number);
-        int computerChoice = (int) ((Math.random() * 10) % 10);
+        input = sc.nextInt();
+        System.out.println("Player got the location on board = " + input);
+        computerChoice = (int) ((Math.random() * 10) % 10);
         System.out.println("Computer got the location on board = " + computerChoice);
-        if (computerChoice == number) {
+        if (computerChoice == input) {
             System.out.println("Player will make choice again");
             System.out.println("Enter number between 1 to 9");
-            number = sc.nextInt();
+            input = sc.nextInt();
         }
     }
+
     private static void checkFreeSpace() {
-        System.out.println("choose index from 1 to 9  to make the move");
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
         if (board[input] == 'x' || board[input] == 'o') {
             System.out.println("Space is not free");
 
         } else if (board[input] != 'x' || board[input] != 'o') {
             System.out.println("Space is free");
-            board[input] = charter;
+            board[input] = userInput;
+            showBoard();
         }
     }
 
     private static void checkFirstMove() {
         int head = 0;
         int check = (int) ((Math.random() * 10) % 2);
+        System.out.println();
         if (check == head) {
             System.out.println("Player will play first");
         } else
